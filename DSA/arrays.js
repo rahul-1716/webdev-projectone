29fce4a5b72a0e5c3fa78c8681e3a76faffcfb29
rahul-1716 <rahulnishad6969@gmail.com>
2026-03-30 01:39:39 +0530

    got a lot to learn

--- DSA/arrays.js
+++ DSA/arrays.js
@@ -1,36 +1,42 @@
 class MyArray{
+
     constructor(){
         this.data = {};
         this.length = 0;
     }
+
     push(item){
         this.data[this.length] = item;
         this.length++;
         return this.length;
     }
+
     pop(){
-        if(this.length === 0) return undefined
+        if(this.length === 0) return undefined;
         const lastItem = this.data[this.length - 1]
-        delete this.data[this.length - 1]
+        delete this.data[this.length - 1];
         this.length--;
-        return lastItem;
+        return lastItem
     }
+
     get(index){
         if(index < 0 || index >= this.length) return undefined;
         return this.data[index];
     }
+
     shift(){
         if(this.length === 0) return undefined;
-        const item = this.data[0];
+        const item = this.data[0]
         for(let i = 0; i < this.length - 1; i++){
             this.data[i] = this.data[i + 1]
         }
-        delete this.data[this.length - 1]
+        delete this.data[this.length - 1];
         this.length--;
         return item;
     }
+
     deleteByIndex(index){
-        if(index < 0 || index >= this.length) return undefined
+        if(index < 0 || index > this.length - 1) return undefined;
         const item = this.data[index]
         for(let i = index; i < this.length - 1; i++){
             this.data[i] = this.data[i + 1]
@@ -39,23 +45,25 @@ class MyArray{
         this.length--;
         return item;
     }
+
     unshift(item){
-        for(let i = this.length - 1; i >= 0; i++){
+        for(let i = this.length - 1; i >= 0; i--){
             this.data[i + 1] = this.data[i];
         }
         this.data[0] = item;
         this.length++;
-        return this.length;
+        return this.data;
     }
-    insert(index, item){
-        if(index < 0 || index > this.length) return undefined;
-        for(let i = this.length - 1; i >= index; i++){
-            this.data[i + 1] = this.data[i]
+
+    indexOf(item){
+    for(let i = 0; i < this.length; i++){
+        if(this.data[i] === item){
+            return i;
         }
-        this.data[index] = item;
-        this.length++;
-        return this.length;
     }
+    return -1;
+}
+
     reverse(){
         let left = 0;
         let right = this.length - 1;
@@ -66,12 +74,22 @@ class MyArray{
             left++;
             right--;
         }
-        return this.data
+        return this.data;
     }
+    indexOf(item){
+        if(index < 0 || index > this.length) return undefined
+        for(let i = 0; i < this.length - 1; i++){
+            if (this.data[i] === item){
+                return i;
+            }
+            return -1;
+        }
+    }
+
     map(callback){
         const newArray = new MyArray();
         for(let i = 0; i < this.length; i++){
-            newArray.push(callback(this.data[i], i, this));
+            newArray.push(callback(this.data[i], i , this))
         }
         return newArray;
     }
@@ -79,8 +97,8 @@ class MyArray{
     filter(callback){
         const newArray = new MyArray();
         for(let i = 0; i < this.length; i++){
-            if(callback(this.data[i], i, this)){
-                newArray.push(this.data[i]);
+            if(callback(this.data[i], i , this)){
+                newArray.push(this.data[i])
             }
         }
         return newArray;
@@ -89,16 +107,22 @@ class MyArray{
     reduce(callback, initialValue){
         let accumulator = initialValue;
         let startIndex = 0;
-
-        if(accumulator === undefined){
+         if(accumulator === undefined){
             if(this.length === 0) return undefined;
-            accumulator = this.data[0];
-            startIndex = 1;
-        }
-
-        for(let i = startIndex; i < this.length; i++){
-            accumulator = callback(accumulator, this.data[i], i , this);
-        }
-        return accumulator;
+                accumulator = this.data[0];
+                startIndex = 1;
+         }
+            for(let i = startIndex; i < this.length; i++){
+                accumulator = callback(accumulator, this.data[i], i ,this)
+            }
+         
+         return accumulator;
     }
+
+      includes(item){
+            for(let i = 0; i < this.length; i++){
+                if(this.data[i] === item) return true;
+            }
+        return false;
+         }
 }
