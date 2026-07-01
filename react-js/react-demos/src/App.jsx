cf065868b0d0c63cedd90db8b672fe5fd74fbfe5
rahul-1716 <rahulnishad6969@gmail.com>
2026-07-01 17:11:56 +0530

    so this is how localstorage works

--- react-js/react-demos/src/App.jsx
+++ react-js/react-demos/src/App.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import './index.css'
-import StyledCard from './components/StyledCard'
+import React, { useEffect, useState } from 'react'
+
 const App = () => {
+  const [friends, setFriends] = useState(["person","one"])
+
+  const addFriend = () => setFriends([...friends, "two"])
+  const removeFriend = () => setFriends(friends.filter(f => f !== 'person'))
   return (
-    <StyledCard />
+    <div>
+      {
+        friends.map(f => (
+          <ul>
+            <li>{f}</li>
+          </ul>
+        ))
+      }
+
+      <button onClick={addFriend}>ADD</button>
+      <button onClick={removeFriend}>REMOVE</button>
+
+      <Example />
+
+    </div>
+    
   )
 }
 
 export default App
+
+
+const Example = () => {
+  const [name, setName] = useState(() => {
+    const savedName = localStorage.getItem('name')
+    return savedName? JSON.parse(savedName) : ''
+  })
+
+useEffect(() => {
+  localStorage.setItem('name', JSON.stringify(name))
+},[name])
+
+  const handleChange = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleClear = () => {
+    setName('')
+  }
+
+  return(
+    <div className="">
+      <h1>YOUR NAME: {name}</h1>
+      <input type="text" value={name} onChange={handleChange} placeholder='ENTER YOUR NAME' />
+      <button onClick={handleClear}>Clear Name</button>
+    </div>
+  )
+}
