57b6e5252cb38f9d17caf1ee2f10530ab0c784f8
rahul-1716 <rahulnishad6969@gmail.com>
2026-07-01 14:04:24 +0530

    conditional rendering

--- react-js/react-demos/src/App.jsx
+++ react-js/react-demos/src/App.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
-import Person from './components/Person'
-import Product from './components/Product'
 
 const App = () => {
   return (
     <div>
-      <Person name="Rahul" age={21} />
-      <Product name="IPHONE" price={1299} />
+      <Weather temperature={16} />
+      <Weather temperature={12} />
+      <Weather temperature={27} />
+      <UserStatus loggedIn={true} isAdmin={false} />
+      <UserStatus loggedIn={false} isAdmin={false} />
+      <UserStatus loggedIn={true} isAdmin={true} />
+      <Greeting timeOfDay={"morning"} />
+      <Greeting timeOfDay={"afternoon"} />
     </div>
   )
 }
 
-export default App
+export default App;
+
+
+const Weather = ({temperature}) => {
+  if(temperature > 25){
+    return <p>It's hot outside</p>
+  }else if(temperature >= 15 && temperature <=25){
+    return <p> It's nice outside</p>
+  }else{
+    return <p>It's Cold Outside</p>
+  }
+}
+
+const UserStatus = ({loggedIn , isAdmin}) => {
+  {
+   if(!loggedIn) return <p>Not Logged in</p>
+   return isAdmin ? <p>Welcome Admin</p> : <p>Welcome User</p>
+   
+  }
+}
+
+const Greeting = ({timeOfDay}) => {
+  if(timeOfDay === "morning") return <p>Good Morning</p>
+  if(timeOfDay === "afternoon") return <p>Good Afternoon</p>
+}
